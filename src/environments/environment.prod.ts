543c5efa8e79d8ba41248a92c5b5e268fca20989
Wilian Morales <willian.moralesch@gmail.com>
2025-03-12 22:49:57 -0500

    Configuración de los environments.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  endPoint: 'http://localhost:5136/api/'  // URL de la API en producción
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  endPoint: 'http://localhost:5136/api/' // URL de la API
+};
