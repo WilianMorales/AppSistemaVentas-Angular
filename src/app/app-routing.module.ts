77e0081774436584aac75b7738dc63a2da6e77cb
Wilian Morales <willian.moralesch@gmail.com>
2025-03-13 23:19:48 -0500

    Creación de componentes, modulos y rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from '../material/material.module';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
+import { HistorialVentaComponent } from './pages/historial-venta/historial-venta.component';
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    DashboardComponent,
+    UsuarioComponent,
+    ProductoComponent,
+    VentaComponent,
+    ReporteComponent,
+    HistorialVentaComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ]
+})
+export class LayoutModule { }
