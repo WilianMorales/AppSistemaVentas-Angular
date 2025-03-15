7358bf017b6fbf45723a36633b466ff3e1c42fa6
Wilian Morales <willian.moralesch@gmail.com>
2025-03-14 21:43:45 -0500

    Implementación del componente usuario y estilos.

--- src/app/layout/pages/usuario/usuario.component.ts
+++ src/app/layout/pages/usuario/usuario.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+
+import { ModalUsuarioComponent } from '../../modales/modal-usuario/modal-usuario.component';
+import { Usuario } from 'src/app/data/interfaces/usuario';
+import { UsuarioService } from 'src/app/data/services/usuario.service';
+import { UtilidadService } from 'src/app/shared/utilidad.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit, AfterContentInit {
+
+  columnasTabla: string[] = ['nombre', 'email', 'rolDescripcion', 'estado', 'acciones']
+  dataListaUsuario = new MatTableDataSource;
+  @ViewChild(MatPaginator) paginacionTabla!: MatPaginator;
+
+  constructor(
+    private _dialog: MatDialog,
+    private _usuarioServicio: UsuarioService,
+    private _utilidadServicio: UtilidadService
+  ) { }
+
+  ngOnInit(): void {
+    this.obtenerUsuario();
+  }
+
+  ngAfterContentInit(): void {
+    this.dataListaUsuario.paginator = this.paginacionTabla;
+  }
+
+  obtenerUsuario() {
+    this._usuarioServicio.getUsuarios().subscribe({
+      next: (data) => {
+        if (data.status) {
+          this.dataListaUsuario.data = data.value;
+        } else {
+          this._utilidadServicio.mostrarAlerta('No se encontraron datos', 'Oops!');
+        }
+      },
+      error: (error) => {
+        this._utilidadServicio.mostrarAlerta('Hubo un error al obtener los usuarios', 'error');
+      }
+    });
+  }
+
+  aplicarFiltroTabla(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataListaUsuario.filter = filterValue.trim().toLocaleLowerCase();;
+  }
+
+  nuevoUsuario() {
+    const dialogRef = this._dialog.open(ModalUsuarioComponent, {
+      disableClose: true, // No permite cerrar el diálogo haciendo clic fuera
+    });
+
+    // Maneja el resultado cuando el diálogo se cierra
+    dialogRef.afterClosed().subscribe(resultado => {
+      if (resultado === true) {
+        this.obtenerUsuario();
+      }
+    });
+  }
+
+  editarUsuario(usuario: Usuario) {
+    this._dialog.open(ModalUsuarioComponent, {
+      data: usuario,
+      disableClose: true,
+    }).afterClosed()
+     .subscribe(resultado => {
+        if (resultado === true) {
+          this.obtenerUsuario();
+        }
+      });
+  }
+
+  eliminarUsuario(_usuario: Usuario) {
+    Swal.fire({
+      title: '¿Estás seguro de eliminar este usuario?',
+      text: _usuario.nombre,
+      icon: 'warning',
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'Sí, eliminar!',
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'No, volver'
+    }).then((res) => {
+      if (res.isConfirmed) {
+        this._usuarioServicio.eliminarUsuario(_usuario.idUsuario).subscribe({
+          next: (data) => {
+            if (data.status) {
+              this._utilidadServicio.mostrarAlerta('Usuario eliminado correctamente', 'Success!');
+              this.obtenerUsuario();
+            } else {
+              this._utilidadServicio.mostrarAlerta('Hubo un error al eliminar el usuario', 'Error');
+            }
+          },
+          error: (error) => {
+            this._utilidadServicio.mostrarAlerta('Hubo un error al eliminar el usuario', 'Error');
+          }
+        });
+      }
+    })
+  }
 
 }
