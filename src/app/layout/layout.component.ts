6bc7b21f03776558e01f9dd1a1885312309af9be
Wilian Morales <willian.moralesch@gmail.com>
2025-03-18 18:13:13 -0500

    refactor: ajustar UI, mejorar estructura HTML, actualizar estilos y optimizar código en varios componentes

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  isMobile: boolean = false;
+  isOpened: boolean = true; // Para escritorio, siempre abierto
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+        // En móviles, cerramos el sidenav por defecto
+        if (this.isMobile) {
+          this.isOpened = false;
+        }
+      });
+  }
 
 }
